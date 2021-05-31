579abd6b33a99e4c0e31724f620522033d712df0
NicoExcalibur <nico.zanz@gmail.com>
2021-05-31 15:56:11 +0200

    change parameters

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+ctx.lineWidth = 50;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
   if (!isDrawing) return; // stop the function from running when there are not moused down
   //   console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   // strart from
   ctx.moveTo(lastX, lastY);
@@ -21,7 +25,20 @@ function draw(e) {
   ctx.stroke();
   lastX = e.offsetX;
   lastY = e.offsetY;
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 }
+
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
   lastX = e.offsetX;
